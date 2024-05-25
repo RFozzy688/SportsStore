24bce8e62103bd6029ac32f1c231acb1f7dd2c6a
RFozzy688 <pokerfozzy@gmail.com>
2024-05-25 04:09:10 +0300

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css'],
+  template: `<div class="bg-success p-3 text-center">
+              This is SportsStore
+            </div>`
 })
-export class AppComponent {
-  title = 'v';
-}
+export class AppComponent {}
