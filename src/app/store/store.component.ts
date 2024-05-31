1b0d56d9719b727732cc7f6009063d88bcdc65fe
RFozzy688 <pokerfozzy@gmail.com>
2024-05-31 15:01:53 +0300

    pagination

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -7,21 +7,45 @@ import { ProductRepository } from "../model/product.repository";
   // moduleId: module.id,
   templateUrl: './store.component.html',
 })
-
 export class StoreComponent {
-  public selectedCategory: string | undefined = "";
+  public selectedCategory: string | undefined = '';
+  public productsPerPage: number = 4;
+  public selectedPage: number = 1;
 
   constructor(private repository: ProductRepository) {}
 
-  get products(): Product[] {    
-    return this.repository.getProducts(this.selectedCategory);
+  get products(): Product[] {
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+
+    return this.repository
+      .getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
   }
 
-  changeCategory(newCategory: string = "") {
+  changeCategory(newCategory: string = '') {
     this.selectedCategory = newCategory;
-}
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageCount(): number {
+    return Math.ceil(this.repository.getProducts(this.selectedCategory)
+      .length / this.productsPerPage);
+  }
+
+  // get pageNumbers(): number[] {
+  //   return Array(Math.ceil(this.repository.getProducts(this.selectedCategory)
+  //     .length / this.productsPerPage)).fill(0).map((x, i) => i + 1);
+  // }
 }
